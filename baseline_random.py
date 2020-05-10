d8575e805ad8b5a74ab421a11f6dcfd95d89b221
hanye4244 <hanye4244@gmail.com>
2020-05-10 09:03:05 +0800

    some change

---
+++ baseline_random.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+'''
+@Author: Ye Han
+@Date: 2020-05-09 18:08:00
+@LastEditTime: 2020-05-09 18:17:27
+@LastEditors: Ye Han
+@Description: 
+@Copyright (c) 2020 - Ye Han
+All rights reserved.
+'''
