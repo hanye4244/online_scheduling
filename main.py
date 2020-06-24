b62038bbad2756f3750475cad94892aea0115ca1
hanye4244 <hanye4244@gmail.com>
2020-06-24 18:36:44 +0800

    up

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 '''
 @Author: Ye Han
 @Date: 2020-05-06 14:59:51
-@LastEditTime: 2020-06-23 18:34:13
+@LastEditTime: 2020-06-24 18:31:37
 @LastEditors: Ye Han
 @Description: The integer programming includes the acceptance rate of PETs.
 @Copyright (c) 2020 - Ye Han
@@ -84,12 +84,12 @@ max_soc = 0.9
 passenger_demand_max = 4
 # passenger_demand_max_list = [4, 3, 2, 1, 0, -1, -2]
 # passenger_demand_max_list = [4]
-V = 300
+V = 1
 # V_list = [1]
 # V_list = [10, 20, 30, 40, 50, 80, 100, 200, 300, 400]
 # worst_case_delay_guarantee_list = [
 #     1, 5, 10, 20, 30, 40, 50, 100, 200, 500, 1000, 1500, 2000]
-worst_case_delay_guarantee_list = [1]
+worst_case_delay_guarantee_list = [400]
 # worst_case_delay_guarantee = 3
 # print('worst_case_delay_guarantee', worst_case_delay_guarantee)
 # for V in V_list:
@@ -195,14 +195,14 @@ for worst_case_delay_guarantee in worst_case_delay_guarantee_list:
     # tag: Parameter print.
     print('V =', V)
     print("worst_case_delay_guarantee = ", worst_case_delay_guarantee)
-    print('profit_list =', profit_list)
+    # print('profit_list =', profit_list)
     profit_mean_list.append(np.mean(profit_list))
     block_cdq_mean_list.append(np.mean(block_cdq_list))
     block_plq_mean_list.append(np.mean(block_plq_list))
     # block_delay_aware_mean_list.append(np.mean(block_delay_aware_list))
     # print(block_plq_list, 'block_plq_list')
 # Tag: Result print.
-print('profit_mean_list', profit_mean_list)
-print('block_cdq_mean_list', block_cdq_mean_list)
+# print('profit_mean_list', profit_mean_list)
+# print('block_cdq_mean_list', block_cdq_mean_list)
 print('block_plq_mean_list', block_plq_mean_list)
 print('-'*100)
