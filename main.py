b2dbc6660182facd0c25174fe4d35d443cf258e1
hanye4244 <hanye4244@gmail.com>
2020-06-26 10:57:49 +0800

    up

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 '''
 @Author: Ye Han
 @Date: 2020-05-06 14:59:51
-@LastEditTime: 2020-06-25 20:49:27
+@LastEditTime: 2020-06-26 10:43:32
 @LastEditors: Ye Han
 @Description:
 @Copyright (c) 2020 - Ye Han
@@ -84,15 +84,16 @@ max_soc = 0.99
 passenger_demand_max = 4
 # passenger_demand_max_list = [4, 3, 2, 1, 0, -1, -2]
 # passenger_demand_max_list = [4]
-# V = 300
+V = 1
 # V_list = [300]
 # V_list = [1, 10, 20, 30, 40, 50, 80, 100, 200, 300, 400]
-# worst_case_delay_guarantee_list = [1, 400]
-V_list = [200, 300, 400]
-worst_case_delay_guarantee = 1
+worst_case_delay_guarantee_list = [1500]
+# 1, 5, 10, 20, 30, 40, 50, 100, 200, 500, 1000, 1500, 2000]
+# V_list = [200, 300, 400]
+# worst_case_delay_guarantee = 1
 # print('worst_case_delay_guarantee', worst_case_delay_guarantee)
-for V in V_list:
-    # for worst_case_delay_guarantee in worst_case_delay_guarantee_list:
+# for V in V_list:
+for worst_case_delay_guarantee in worst_case_delay_guarantee_list:
         # for passenger_demand_max in passenger_demand_max_list:
         # Initialize pet.
     profit_list = []
